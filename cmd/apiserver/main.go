849c75f9517b76ad09508000b97db86a7afd726c
v.toropov <v.toropov@rambler-co.ru>
2020-05-03 20:33:44 +0300

    save

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"go_rest/internal/app/apiserver"
+	"log"
+)
+
+var (
+	configPath string
+	)
+
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	fmt.Println(config)
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
